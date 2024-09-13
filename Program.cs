b38f2c34302ef063c69e39f71ba5874323790bf1
Daniel Fernandes <fernandesotero@proton.me>
2024-09-13 19:26:21 -0300

    Desafio concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado!
+
+Smartphone iphone = new Iphone(numero: "9389", modelo: "15 PRO", imei: "9999", memoria: 128);
+Smartphone nokia = new Nokia(numero: "7456", modelo: "3200", imei: "1111", memoria: 64);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("VSCode");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Visual Studio");
